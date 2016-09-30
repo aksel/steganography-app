9f7a91e90d06dc0e48281ad774b9ee88a6f86481
Aksel Torgard <aksel@akseltorgard.com>
2016-09-30 12:21:46 +0200

    Basic Steganography encoder and decoder functional.

--- app/src/main/java/com/akseltorgard/steganography/utils/SteganographyUtils.java
+++ app/src/main/java/com/akseltorgard/steganography/utils/SteganographyUtils.java
@@ -2,11 +2,100 @@ package com.akseltorgard.steganography.utils;
 
 public class SteganographyUtils {
 
+    static final int LSB = 1;
+
+    /**
+     * Breaks down message into bits. Each bit replaces the LSB of a pixel.
+     * The minimum number of pixels is 32 + (message.length*8), as the length of the message is
+     * also encoded into the pixels, and each bit requires one pixel.
+     * @param pixels Pixels as (A)RGB integers: R=0xFF0000, G=0x00FF00, B=0x0000FF
+     * @param message Message to encode.
+     * @return Pixels encoded with message.
+     */
     public static int[] encode(int[] pixels, String message) {
+        byte[] data = message.getBytes();
+
+        int requiredLength = data.length * 8 + 32;
+
+        if (requiredLength > pixels.length) {
+            throw new IllegalArgumentException("Message is too long to fit into pixels.");
+        }
+
+        //Insert length into data
+        {
+            byte[] dataWithLength = new byte[4 + data.length];
+
+            int dataLength = data.length;
+            for (int i = 0; i < 4; i++) {
+                dataWithLength[i] = (byte)(dataLength & 0xff);
+                dataLength >>>= 8;
+            }
+
+            System.arraycopy(data, 0, dataWithLength, 4, data.length);
+            data = dataWithLength;
+        }
+
+        int pixelIndex = 0;
+
+        //For each byte of data, insert its 8 bits into the LSB of 8 pixels.
+        for (byte b : data) {
+            for (int i = 0; i < 8; i++, pixelIndex++) {
+                //Get pixel without its LSB
+                int pixel = pixels[pixelIndex] & ~LSB;
+                //OR the LSB of current byte into pixel
+                pixel |= b & LSB;
+                //Bit-shift current byte into next position
+                b >>>= 1;
+                pixels[pixelIndex] = pixel;
+            }
+        }
+
         return pixels;
     }
-
+    /**
+     * Decodes data out of pixels, and constructs a String out of it. .
+     * Each byte of data is constructed out of the LSB of 8 consecutive pixels.
+     * @param pixels Pixels as (A)RGB integers: R=0xFF0000, G=0x00FF00, B=0x0000FF
+     * @return Decoded data.
+     */
     public static String decode(int[] pixels) {
-        return "Hello world!";
+
+        int pixelIndex = 0;
+
+        //Decode length;
+        int length = decodeBitsFromPixels(pixels, 32, pixelIndex);
+        pixelIndex += 32;
+
+        byte[] data = new byte[length];
+
+        for (int byteIndex = 0; byteIndex < length; byteIndex++, pixelIndex+=8) {
+            data[byteIndex] = (byte) decodeBitsFromPixels(pixels, 8, pixelIndex);
+        }
+
+        return new String(data);
+    }
+
+    /**
+     * Decodes specified number of bits out of pixels, returns them as a single integer.
+     * @param pixels Pixels as (A)RGB integers: R=0xFF0000, G=0x00FF00, B=0x0000FF
+     * @param numberOfBits Number of bits to decode.
+     * @param pixelIndex Index to start at.
+     * @return Decoded integer.
+     */
+    private static int decodeBitsFromPixels(int[] pixels,
+                                            int numberOfBits,
+                                            int pixelIndex) {
+
+        int decodedValue = 0;
+
+        for (int i = 0; i < numberOfBits; i++, pixelIndex++) {
+            //Get bit
+            int bit = pixels[pixelIndex] & LSB;
+            //Shift into position
+            decodedValue |= bit << i;
+        }
+
+        return decodedValue;
+
     }
 }
--- app/src/test/java/SteganographyTest.java
+++ app/src/test/java/SteganographyTest.java
@@ -1,6 +1,54 @@
-/**
- * Created by Aksel on 30-09-2016.
- */
+import com.akseltorgard.steganography.utils.SteganographyUtils;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import java.util.Arrays;
 
 public class SteganographyTest {
+
+    final int OPAQUE_WHITE = 0xFFFFFFFF;
+    final String MESSAGE = "Hello world! lorem ipsum bla bla bla bla";
+
+    /**
+     * Int array of opaque white pixels. Length is 8 + 32.
+     * 32 bytes for the length (32 bit int), and 8 bytes for each letter in MESSAGE.
+     * @return Array of opaque white pixels.
+     */
+    private int[] getUnencodedPixels() {
+        int[] pixels = new int[MESSAGE.length()*8 + 32];
+        Arrays.fill(pixels, OPAQUE_WHITE);
+
+        return pixels;
+    }
+
+    private int[] getEncodedPixels() {
+        return SteganographyUtils.encode(getUnencodedPixels(), MESSAGE);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testEncodeMessageTooLong() {
+        int[] pixels = getUnencodedPixels();
+
+        pixels = SteganographyUtils.encode(pixels, MESSAGE + "Message too long to fit into pixels.");
+    }
+
+    @Test
+    public void testEncodeChangesPixels() {
+        int[] pixels = getUnencodedPixels();
+
+        pixels = SteganographyUtils.encode(pixels, MESSAGE);
+
+        Assert.assertEquals(false, Arrays.equals(pixels, getUnencodedPixels()));
+    }
+
+    @Test
+    public void testDecode() {
+        int[] pixels = getEncodedPixels();
+
+        String decodedMessage = SteganographyUtils.decode(pixels);
+
+        Assert.assertEquals(MESSAGE, decodedMessage);
+    }
 }
